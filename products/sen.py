6588a61322254de96379bd52fd6d748a8bc04db3
Shuo Kong <shuo.kong@yale.edu>
2017-09-27 19:52:08 -0400

    imsub subregion make spectra

--- products/sen.py
+++ products/sen.py
@@ -14,6 +14,6 @@ rmsdata = hdu1.data[0,:10,:,:]
 for j in range(len(hdu2.data[0,0,:,0])):
     for k in range(len(hdu2.data[0,0,0,:])):
 #        if not ((j>3014 and j<3014+400 and k>1427 and k<1427+400) or (j>1303 and j<1303+400 and k>911 and k<911+400) or (j>760 and j<760+400 and k>818 and k<818+400)):
-        hdu2.data[0,:,j,k] = np.std(rmsdata[:,j,k])
-hdu2.writeto('test.fits', output_verify='exception', clobber=True, checksum=False)
+        hdu2.data[0,:,j,k] = np.nanstd(rmsdata[:,j,k])
+hdu2.writeto('12co_pix_2_Tmb_sens.fits', output_verify='exception', clobber=True, checksum=False)
 
